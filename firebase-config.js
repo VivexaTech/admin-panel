401c56fef03f8054e54339c7ec659d8f60c10f77
Vivexa Tech <vivexatech@gmail.com>
2025-08-10 16:36:57 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 // firebase-config.js
 var firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+
+  apiKey: "AIzaSyArx3eeAj98XkXJqMtITF2I00sjm-LQDqs",
+  authDomain: "vivexa-admin.firebaseapp.com",
+  projectId: "vivexa-admin",
+  storageBucket: "vivexa-admin.firebasestorage.app",
+  messagingSenderId: "68717817629",
+  appId: "1:68717817629:web:a2a6dc32979f2b2d1fbbf4",
 };
